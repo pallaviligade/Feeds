6f0a8acb78e25c8a70930d28e4f82f41201d1734
Pallavi <pallavi0204@gmail.com>
2023-05-10 16:04:27 +0200

    created new file for feedcahe policy

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,26 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Pallavi on 10.05.23.
+//
+
+import Foundation
+
+private final  class  FeedCachePolicy {
+    private init () {}
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+   static private var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+   static func validate(_ timestamp: Date, against date:Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
+            return false
+        }
+        return date < maxCacheAge
+    }
+    
+    
+}
