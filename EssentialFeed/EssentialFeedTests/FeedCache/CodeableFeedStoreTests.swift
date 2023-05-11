5b216cf311890b22cab55d7228a152cbb77f0243
Pallavi <pallavi0204@gmail.com>
2023-05-11 13:53:52 +0200

    revivals  2 from  empty cache no side effect

--- EssentialFeed/EssentialFeedTests/FeedCache/CodeableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CodeableFeedStoreTests.swift
@@ -31,8 +31,27 @@ final class CodeableFeedStoreTests: XCTestCase {
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrive_hasNosideEffectsOnemptyCacheTwice()
+    {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "wait till expectation")
         
-      
+       
+        sut.retrival { firstResult in
+            sut.retrival { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty):
+                    break
+                default:
+                    XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult) instead")
+                    
+                }
+                exp.fulfill()
+            }
+        }
+        wait(for: [exp], timeout: 1.0)
     }
    
     
