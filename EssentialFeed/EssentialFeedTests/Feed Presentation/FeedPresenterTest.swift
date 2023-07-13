92683e2ccb60afb3efa1628beb4fc55e2f8e1bcb
Pallavi <pallavi0204@gmail.com>
2023-07-13 11:12:05 +0200

    created SUT method

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTest.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTest.swift
@@ -16,14 +16,20 @@ class FeedPresenter {
 class FeedPresenterTest: XCTestCase{
     
     func test_init_doesNotSendMessagesToView() {
-        let view = viewSpy()
-        
-        //Action initalizer
-        let _ = FeedPresenter(view: view)
-        XCTAssertTrue(view.message.isEmpty,"Expected no view messages")
+      let (_, view) = makeSUT()
+      XCTAssertTrue(view.message.isEmpty,"Expected no view messages")
         
     }
     //MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: viewSpy) {
+        let view = viewSpy()
+        //Action initalizer
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+    }
     private class viewSpy {
         var message = [Any]()
     }
