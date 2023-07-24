2766823c6b7f0ff5acc784e1757bcc2e7b4b378f
Pallavi <pallavi0204@gmail.com>
2023-07-24 15:47:12 +0200

    adding remote Feedloader file to  production

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+public final class RemoteFeedImageDataLoader {
+    public enum Error: Swift.Error {
+        case invaildData
+        case connectivity
+    }
+    private let httpClient: Httpclient
+    
+   public init(client: Httpclient) {
+        httpClient = client
+    }
+    
+    private struct HTTPTaskWrapper: FeedImaegDataLoaderTask {
+        
+        let wrapped: HTTPClientTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+        
+    }
+    
+   public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImaegDataLoaderTask {
+        
+        
+      return HTTPTaskWrapper(wrapped: httpClient.get(from: url) { [weak self] result in
+            guard self != nil else {
+                return
+            }
+            switch result {
+            case let .success((data, response)):
+                if response.statusCode == 200, !data.isEmpty {
+                    completion(.success(data))
+                }else {
+                    completion(.failure(Error.invaildData))
+                }
+            case let .failure(error):
+                completion(.failure(error))
+            }
+        })
+    }
+    
+}
